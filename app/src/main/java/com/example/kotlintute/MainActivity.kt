9996b54fefb554bc837f0d53116ac5c2f04e828c
Shadini Kalansooriya <41547405+ShadiniKalansooriya@users.noreply.github.com>
2020-05-28 15:24:21 +0530

    Age Calculator done

---
+++ app/src/main/java/com/example/kotlintute/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kotlintute
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun onSubmit(view: View){
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        textViewAge.text = "" +(currentYear-editTextYear.text.toString().toInt());
+    }
+}
